ad8b4fd4cda691834b9563fa61a4ee37761b6cd6
yelin <yelinim8@gmail.com>
2025-11-27 21:16:00 -0800

    Add a printing message for ML API

--- api/ml_api.py
+++ api/ml_api.py
@@ -12,6 +12,7 @@ smoother = Smoother()
 class ImageData(BaseModel):
     image: str
 
+print('ML API is running')
 @app.post("/predict-cnn")
 def predict(data: ImageData):
     x = decode_and_preprocess(data.image)
